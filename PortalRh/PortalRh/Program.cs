4ad07d4d106632bdb1f95abb15b5d465d7e38c39
Carlos Martinez <cmartinezm@outlook.com>
2024-04-18 23:50:11 -0500

    Se agraga migracion y funcionamient Identity

--- PortalRh/PortalRh/Program.cs
+++ PortalRh/PortalRh/Program.cs
@@ -1,7 +1,12 @@
 using Blazorise.Bootstrap5;
 using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PortalRh.Data;
+using PortalRh.Areas.Identity;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +15,17 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
-
-
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+
+
+
+
 
 AddBlazorise(builder.Services);
 
